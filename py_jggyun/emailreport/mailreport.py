94db95f17dd0f1701e8df8e35c7b66220bb30856
guojiong <328905689@qq.com>
2020-08-12 14:41:16 +0800

    sendemail

---
+++ py_jggyun/emailreport/mailreport.py
@@ -0,0 +1,57 @@
+'''
+Created on 2020年8月11日
+
+@author: Administrator
+'''
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import time
+
+
+class EmailClass():
+    def __init__(self):
+        # 邮件发送方地址
+        self.fromaddr = 'jiong.guo@qyt1902.com'
+        self.frompasswd = 'Qyt0304.' # 部分邮件为授权码
+        # 邮件接收方地址
+        self.toaddr = ['328905689@qq.com', '']  # []包裹，可群发
+        self.message = MIMEMultipart('mixed')
+    
+    def send_content(self, title, report):
+        self.message['Subject'] = title
+        self.message['From'] = self.fromaddr
+        self.message['To'] = self.toaddr[0]
+        with open(report, 'rb') as f:
+            content = f.read()
+        self.message.attach(MIMEText(content, 'html', 'utf-8'))
+        self.html = self.addAttach(report, '%s_%s.html' % (title, time.strftime('%H-%m-%d_%H-%M-%S')))
+        self.message.attach(self.html)
+        
+    def send(self, title, apath):
+        self.send_content(title, apath)
+        try:
+            server = smtplib.SMTP('smtp.exmail.qq.com')
+            server.login(self.fromaddr, self.frompasswd)
+            server.sendmail(
+                from_addr=self.fromaddr, 
+                to_addrs=self.toaddr, 
+                msg=self.message.as_string())
+            print('Send Report Success')
+            server.quit()
+        except smtplib.SMTPException as e:
+            print('error', e)
+    
+    # 添加附件
+    def addAttach(self, apath, filename):
+        with open(apath, 'rb') as f:
+            attach = MIMEBase('application','utf-8')
+            attach.set_payload(f.read())
+            attach.add_header('Content-Disposition', 'attachment', filename=filename)
+            encoders.encode_base64(attach)
+            return attach
+            
+if __name__ == '__main__':
+    EmailClass().send(r"Order Test Report", r'D:\AutoFuncSysTest\mytest_rep\py_jggyun\ui\reports\report_09-08-12_09-54-59.html')
--- py_jggyun/ui/run.py
+++ py_jggyun/ui/run.py
@@ -4,15 +4,41 @@ Created on 2020年8月7日
 @author: Administrator
 '''
 import os
-from ui.utils.config import REPORT_PATH
+from ui.utils.config import REPORT_PATH, BASE_PATH
 import time
 import unittest
 from ui.test.case.order.test_login import TestLogin
-from unittest.runner import TextTestRunner
+from HTMLTestRunner_cn import HTMLTestRunner
+from emailreport.mailreport import EmailClass
 
 report = os.path.join(REPORT_PATH, 'report_%s.html' % time.strftime('%H-%m-%d_%H-%M-%S'))
 
-suite = unittest.TestSuite()
-suite.addTest(TestLogin)
+def suitebyadd():
+    suite = unittest.TestSuite()
+    suite.addTest(TestLogin("testLogin"))
+    return suite
 
-runner = TextTestRunner
+def suitebypath(*args):
+    case_path = os.path.join(BASE_PATH, *args)
+    suite = unittest.defaultTestLoader.discover(case_path)
+    return suite
+
+# runner = TextTestRunner()
+path = ("test", "case", "order")
+title="订单中台测试报告"
+with open(report, 'wb') as f:
+    runner = HTMLTestRunner(
+        stream=f, 
+        verbosity=2, 
+        title=title, 
+        description="", 
+        is_thread=True, 
+        retry=1, 
+        save_last_try=True)
+    runner.run(suitebypath(*path))
+    f.close()
+# 打开测试报告
+os.startfile(report)
+
+# 发送测试报告
+EmailClass().send(title, report)
